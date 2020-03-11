d7754fdda86cd55b34a36a7dc21e614b4be3b749
Sam Sauer <s.sauer@syzygy.de>
2020-03-11 18:08:25 +0100

    Fix server connection port

--- client/gameClient.js
+++ client/gameClient.js
@@ -7,9 +7,9 @@ import { frameState, releaseKeys, currentState } from './input.js'
 import PlayerInput from '../common/commands/PlayerInput.js'
 
 const client = new nengi.Client(nengiConfig, 100)
-let serverAdress = 'ws://62.75.139.27:8079';
+let serverAdress = 'ws://62.75.139.27:8001';
 if (process.env.NODE_ENV == 'development') {
-    serverAdress = 'ws://localhost:8001'
+    serverAdress = 'ws://localhost:8079'
 }
 console.info("Connect to Server at:", serverAdress);
 client.connect(serverAdress)
