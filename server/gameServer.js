f6506b0785278721c3b921191f33c2f32da3ad1b
samsour <sam.sauer@hotmail.de>
2020-03-12 21:43:50 +0100

    Add new server setup

---
+++ server/gameServer.js
@@ -0,0 +1,69 @@
+import nengi from 'nengi'
+import nengiConfig from '../common/nengiConfig.js'
+import instanceHookAPI from './instanceHookAPI.js'
+import NetLog from '../common/NetLog.js'
+import PlayerCharacter from '../common/PlayerCharacter.js'
+import Asteroid from '../common/Asteroid.js'
+import Identity from '../common/Identity.js'
+
+const instance = new nengi.Instance(nengiConfig, { port: 8079 })
+instanceHookAPI(instance)
+
+/* serverside state here */
+const entities = new Map()
+
+instance.on('connect', ({ client, callback }) => {
+    /* client init logic & state can go here */
+    callback({ accepted: true, text: 'Welcome!' })
+    instance.message(new NetLog('hello world'), client)
+    const entity = new PlayerCharacter()
+    instance.addEntity(entity)
+    instance.message(new Identity(entity.nid), client)
+    entities.set(entity.nid, entity)
+    client.entity = entity
+    client.view = {
+        x: entity.x,
+        y: entity.y,
+        halfWidth: 500,
+        halfHeight: 500
+    }
+})
+
+instance.on('disconnect', client => {
+    entities.delete(client.entity.nid)
+    instance.removeEntity(client.entity)
+})
+
+/* on('command::AnyCommand', ({ command, client }) => { }) */
+instance.on('command::PlayerInput', ({ command, client }) => {
+    const { up, down, left, right, rotation, delta } = command
+    const { entity } = client
+    const speed = 200
+    if (up) {
+        entity.y -= speed * delta
+    }
+    if (down) {
+        entity.y += speed * delta
+    }
+    if (left) {
+        entity.x -= speed * delta
+    }
+    if (right) {
+        entity.x += speed * delta
+    }
+    entity.rotation = rotation
+})
+
+const update = (delta, tick, now) => {
+    instance.emitCommands()
+    /* serverside logic can go here */
+    instance.clients.forEach(client => {
+        client.view.x = client.entity.x
+        client.view.y = client.entity.y
+    })
+    instance.update()
+}
+
+export {
+    update
+}
--- server/serverMain.js
+++ server/serverMain.js
@@ -1,28 +1,26 @@
-import GameInstance from './GameInstance';
-import nengiConfig from '../common/nengiConfig';
-
-const gameInstance = new GameInstance(/*args*/)
+import nengiConfig from '../common/nengiConfig.js'
+import { update } from './gameServer.js'
 
 const hrtimeMs = function() {
-    let time = process.hrtime()
+    const time = process.hrtime()
     return time[0] * 1000 + time[1] / 1000000
 }
 
 let tick = 0
 let previous = hrtimeMs()
-let tickLengthMs = 1000 / nengiConfig.UPDATE_RATE
+const tickLengthMs = 1000 / nengiConfig.UPDATE_RATE
 
 const loop = function() {
-    let now = hrtimeMs()
+    const now = hrtimeMs()
     if (previous + tickLengthMs <= now) {
-        let delta = (now - previous) / 1000
+        const delta = (now - previous) / 1000
         previous = now
         tick++
 
-        //let start = hrtimeMs() // uncomment to benchmark
-        gameInstance.update(delta, tick, Date.now())
-        //let stop = hrtimeMs()
-        //console.log('game update took', stop-start, 'ms')
+        //const start = hrtimeMs() // uncomment to benchmark
+        update(delta, tick, Date.now())
+        //const stop = hrtimeMs()
+        //console.log('update took', stop-start, 'ms')
     }
 
     if (hrtimeMs() - previous < tickLengthMs - 4) {
