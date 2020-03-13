8c32279ddc4cfcb6e448c4c4e8b3ef4b70ac582e
samsour <sam.sauer@hotmail.de>
2020-03-14 00:06:43 +0100

    Refactor gameServer

--- server/gameServer.js
+++ server/gameServer.js
@@ -16,9 +16,11 @@ const entities = new Map()
 asteroidSystem.populate(instance, 50);
 
 instance.on('connect', ({ client, callback }) => {
-    /* client init logic & state can go here */
+    /* client init logic & state */
     callback({ accepted: true, text: 'Welcome!' })
     instance.message(new NetLog('hello world'), client)
+
+    // create new PlayerCharacter
     const entity = new PlayerCharacter()
     instance.addEntity(entity)
     instance.message(new Identity(entity.nid), client)
@@ -31,8 +33,7 @@ instance.on('connect', ({ client, callback }) => {
         halfHeight: 1000
     }
     // instanciate an own weaponSystem (handling cooldowns) for each player
-    const ws = new WeaponSystem()
-    client.ws = ws
+    client.weaponSystem = new WeaponSystem()
 })
 
 instance.on('disconnect', client => {
@@ -41,6 +42,11 @@ instance.on('disconnect', client => {
 })
 
 /* on('command::AnyCommand', ({ command, client }) => { }) */
+
+/**
+ * Player Input logic contains moving & shooting
+ * @param {PlayerInput}
+ */
 instance.on('command::PlayerInput', ({ command, client }) => {
     const { up, down, left, right, mouseDown, mouseX, mouseY, rotation, delta } = command
     const { entity } = client
@@ -57,7 +63,8 @@ instance.on('command::PlayerInput', ({ command, client }) => {
     if (right) {
         entity.x += speed * delta
     }
-    if (mouseDown && client.ws.fire()){
+
+    if (mouseDown && client.weaponSystem.fire()){
         const explosion = new Explosion()
         explosion.x = mouseX
         explosion.y = mouseY
@@ -66,20 +73,34 @@ instance.on('command::PlayerInput', ({ command, client }) => {
             instance.removeEntity(explosion)
         },250)
     }
+    
     entity.rotation = rotation
 })
 
+/**
+ * Game loop that contains the serverside game logic
+ * @param {number} delta - time difference between the previous frame that was drawn and the current frame
+ * @param {number} tick - how many game loops are done so far
+ * @param {Date.now} now - represents the current time as a number
+ */
 const update = (delta, tick, now) => {
     instance.emitCommands()
     /* serverside logic can go here */
+
+    // update all clients
     instance.clients.forEach(client => {
+        // update client positions
         client.view.x = client.entity.x
         client.view.y = client.entity.y
 
-         // update weaponSystem for each client
-         client.ws.update(delta)
+         // update client weaponSystem
+         client.weaponSystem.update(delta)
     })
+
+    // update asteroid system
     asteroidSystem.update(delta)
+
+    /* serversode logic end */
     instance.update()
 }
 
