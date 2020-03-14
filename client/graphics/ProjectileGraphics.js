d98d7cac4021a591def0b19ed5ec61ff74fe9e53
samsour <sam.sauer@hotmail.de>
2020-03-14 19:21:36 +0100

    Adjust projectile graphic

--- client/graphics/ProjectileGraphics.js
+++ client/graphics/ProjectileGraphics.js
@@ -5,7 +5,7 @@ class ProjectileGraphics extends Container {
         super()
 
         const sprite = Sprite.from('/images/laser_t1.png')
-        sprite.scale.set(3, 3)
+        sprite.scale.set(2, 3)
         sprite.anchor.set(0.5, 0.5)
         sprite.rotation = 0.5 * Math.PI
         this.addChild(sprite)
