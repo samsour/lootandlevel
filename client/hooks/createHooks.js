6082e3fd826487b0b6268e3b1b34677a8fb28056
samsour <sam.sauer@hotmail.de>
2020-03-14 19:22:45 +0100

    Fix projectileHook

--- client/hooks/createHooks.js
+++ client/hooks/createHooks.js
@@ -1,11 +1,13 @@
 import playerHooks from './playerHooks.js'
 import asteroidHooks from './asteroidHooks.js'
 import explosionHooks from './explosionHooks'
+import projectileHooks from './projectileHooks'
 
 export default (state) => {
     return {
         'PlayerCharacter': playerHooks(state),
         'Asteroid': asteroidHooks(state),
-        'Explosion': explosionHooks(state)
+        'Explosion': explosionHooks(state),
+        'Projectile': projectileHooks(state),
     }
 }
